0b97f5f3631d9892ad15676c3faa68ff1da527bc
Roi Herman Pascual <61074493+roihp@users.noreply.github.com>
2026-02-25 11:01:26 -0500

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,3 +1,5 @@
+import os
+import json
 from azure.ai.ml import MLClient
 from azure.identity import DefaultAzureCredential
 from azure.ai.ml.entities import ManagedOnlineEndpoint, ManagedOnlineDeployment
