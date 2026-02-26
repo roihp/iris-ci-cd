62f480c97bc426f8b8bcc99daec15c3c8ec9f000
Roi Herman Pascual <61074493+roihp@users.noreply.github.com>
2026-02-25 19:11:03 -0500

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -15,7 +15,12 @@ endpoint_name = "iris-endpoint-" + str(uuid.uuid4())[:8]
 deployment_name = "blue"
 
 credential = DefaultAzureCredential()
-ml_client = MLClient.from_config(credential)
+ml_client = MLClient(
+    credential=credential,
+    subscription_id=os.environ["AZURE_SUBSCRIPTION_ID"],
+    resource_group_name=os.environ["AZURE_RESOURCE_GROUP"],
+    workspace_name=os.environ["AZURE_WORKSPACE_NAME"]
+)
 
 # Create endpoint
 endpoint = ManagedOnlineEndpoint(
